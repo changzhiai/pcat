e9eaa8a69e0af1e601fb7f0f97b37fb2650572a8
Changzhi <changzhiai@outlook.com>
2022-03-10 14:21:23 +0100

    add some tests

---
+++ my_project/proj2/test/Pourbaix.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 18 16:56:18 2022
+
+@author: changai
+"""
+
+"""
+Elementary steps:
+    
+CO2(g) + * + H+ + e- -> HOCO*   (dG1)
+dG1 = G_HOCO* - G_CO2g - G* - 1/2 G_H2g
+Gb_HOCO = dG1
+
+HOCO* + H+ + e- -> CO* + H2O(g) (dG2)
+dG2 = G_CO* + G_H2Og - G_HOCO* - 1/2 G_H2g
+Gb_CO_ref_e = dG2 - dG1 # reference electrochemical step
+
+CO* -> CO(g) + *                  (dG3)
+dG3 = G_COg + G* - G_CO*
+Gb_CO = -dG3
+
+H+ + e- + * -> H*                 (dG4)
+dG4 = G_H - G* - 1/2 G_H2g
+Gb_H = dG4
+
+H2O(g) + * -> OH* + H+ + e-       (dG5)
+dG5 = G_OH + 1/2 G_H2g - G* - G_H2O(g)
+Gb_OH = dG5
+
+
+
+
+G = E_dft + E_zpe + C_p - TS
+
+delta_G(U, pH) = delta_G(U=0, pH=0) - ne * U + n * k_B * T * pH * ln(10)
+Here e is -1, reference Andrew`s paper
+μ( H+(aq)) + μ(e-) = 1/2μH2(g) - eUSHE + kBT ln(10) pH from Heine`s theis
+
+Note:   alpha = np.log(10) * self.kT
+        if name == 'e-':
+            energy = -U
+        elif name == 'H+(aq)':
+            energy = -pH * alpha
+        , which is from ase
+    
+"""
+
+from pcat.lib.io import pd_read_excel
+import numpy as np
+import pcat.utils.constants as const
+import matplotlib.pyplot as plt
+import pandas as pd
+plt.rcParams.update({'mathtext.default':  'regular', 'figure.dpi': 150})
+
+def pourbaix_diagram(U, pH):
+    """Get pourbaix diagram as a function of applied potential and pH value"""
+    df = pd_read_excel(filename=xls_name, sheet=sheet_name_dGs)
+    df = df.iloc[10]
+    # surface = df['Surface']
+    dG1 = df['dG1']
+    dG2 = df['dG2']
+    dG3 = df['dG3']
+    dG4 = df['dG4']
+    dG5 = df['dG5']
+    
+    # N, M = 20*4, 20*4
+    # N, M = 20, 20
+    # U_model = np.linspace(min(U), max(U), N)
+    # pH_model = np.linspace(min(pH), max(pH), M)
+    # pH_model = [0]
+    
+    kB = const.kB
+    T = 297.15
+    
+    N, M = 200, 200
+    
+    # pH = 0
+    U_model = np.linspace(min(U), max(U), N)
+    if type(pH) == int or type(pH) == float:
+        pH_model = [pH]
+    else:
+        pH_model = np.linspace(min(pH), max(pH), M)
+    Us = []
+    pHs = []
+    Gb_HOCOs = []
+    Gb_CO_ref_es = []
+    Gb_COs = []
+    Gb_Hs = []
+    Gb_OHs = []
+    colors = []
+    Us_acc = np.zeros(4)
+    pHs_acc = np.zeros(4)
+    count = np.zeros(4)
+    for i, ph in enumerate(pH_model):
+        for j, u in enumerate(U_model):
+            Gb_HOCO = dG1 + u + kB * T * ph * np.log(10)
+            Gb_CO_ref_e = dG2 - dG1
+            Gb_CO = -dG3
+            Gb_H = dG4 + u + kB * T * ph * np.log(10)
+            Gb_OH = dG5 - u - kB * T * ph * np.log(10)
+            # print(u, Gb_HOCO)
+            # plt.scatter(u, Gb_HOCO)
+            Us.append(u)
+            pHs.append(ph)
+            
+            # Gb_HOCO = Gb_HOCO/4
+            # Gb_CO = Gb_CO/2
+            # Gb_H = Gb_H
+            # Gb_OH = Gb_OH/2
+            
+            Gb_HOCOs.append(Gb_HOCO)
+            Gb_CO_ref_es.append(Gb_CO_ref_e)
+            Gb_COs.append(Gb_CO)
+            Gb_Hs.append(Gb_H)
+            Gb_OHs.append(Gb_OH)
+            
+            min_dot = min(Gb_HOCO, Gb_CO, Gb_H, Gb_OH)
+            if min_dot == Gb_HOCO:
+                color = 'blue'
+                Us_acc[0] += u
+                pHs_acc[0] += ph
+                count[0] += 1
+            elif  min_dot == Gb_CO:
+                color = 'orange'
+                Us_acc[1] += u
+                pHs_acc[1] += ph
+                count[1] += 1
+            elif  min_dot == Gb_H:
+                color = 'green'
+                Us_acc[2] += u
+                pHs_acc[2] += ph
+                count[2] += 1
+            elif  min_dot == Gb_OH:
+                color = 'brown'
+                Us_acc[3] += u
+                pHs_acc[3] += ph
+                count[3] += 1
+            colors.append(color)
+            
+    if type(pH) == int or type(pH) == float:
+        plt.plot(Us, Gb_HOCOs, label='Gb_HOCOs')
+        # plt.plot(Us, Gb_CO_ref_es, label='Gb_CO_ref_es')
+        plt.plot(Us, Gb_COs, label='Gb_COs')
+        plt.plot(Us, Gb_Hs, label='Gb_Hs')
+        plt.plot(Us, Gb_OHs, label='Gb_OHs')
+        plt.xlabel('$U_{SHE}$')
+        plt.ylabel('$\Delta G$ (eV/per adsorbate)')
+        plt.legend()
+        plt.show()
+    else:
+        plt.scatter(pHs, Us, c=colors, marker='o', zorder=2)
+        plt.xlabel('pH')
+        plt.ylabel('$U_{SHE}$ (V)')
+        for i, txt in enumerate(['Gb_HOCOs', 'Gb_COs', 'Gb_Hs', 'Gb_OHs']):
+            x = pHs_acc[i]/count[i]
+            y = Us_acc[i]/count[i] 
+            plt.text(x, y, txt, horizontalalignment='center')
+        
+    tuples = {'Us': Us,
+              'pHs': pHs,
+              'Gb_HOCOs': Gb_HOCOs,
+              'Gb_CO_ref_es': Gb_CO_ref_es,
+              'Gb_COs': Gb_COs,
+              'Gb_Hs': Gb_Hs,
+              'Gb_OHs': Gb_OHs,
+              'Colors': colors,
+              }
+    df_FE = pd.DataFrame(tuples)
+    
+        
+if __name__ == '__main__':
+    
+    system_name = 'collect_vasp_PdHy_v3'
+    
+    xls_name = f'../data/{system_name}.xlsx'
+    fig_dir = '../figures'
+    
+    sheet_name_dGs = 'dGs'
+    
+    U = [-2, 3]
+    pH = 0
+    pourbaix_diagram(U, pH)
+    
+    pH = [0, 14]
+    pourbaix_diagram(U, pH)
+    
+    
---
+++ my_project/proj2/test/db2csv_FE_SR.py
@@ -0,0 +1,346 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  4 13:55:28 2022
+
+@author: changai
+"""
+# import sys
+# sys.path.append("../../../")
+
+from ase.db import connect
+import pandas as pd
+from pcat.lib.io import pd_read_excel
+from pcat.free_energy import CO2RRFED
+from pcat.scaling_relation import ScalingRelation
+import matplotlib.pyplot as plt
+import os
+from pcat.selectivity import Selectivity
+
+def db2xls(system_name, xls_name, db, sheet_free_energy, sheet_binding_energy, sheet_selectivity):
+    """
+    convert database into excel
+    
+    totally produce four sheets
+    """
+    E_H2g = -7.158 # eV
+    E_CO2g = -18.459
+    E_H2Og = -12.833
+    E_COg = -12.118
+    
+    # G_gas = E_pot + E_ZPE + C_Idel_gas - TS + Ecorr_overbing + E_solvent
+    Gcor_H2g = 0.274 + 0.091 - 0.403 + 0.1 + 0
+    Gcor_CO2g = 0.306 + 0.099 - 0.664 + 0.3 + 0
+    Gcor_H2Og = 0.572 + 0.104 - 0.670
+    Gcor_COg = 0.132 + 0.091 - 0.669
+    G_H2g = E_H2g + Gcor_H2g
+    G_CO2g = E_CO2g + Gcor_CO2g
+    G_H2Og = E_H2Og + Gcor_H2Og
+    G_COg = E_COg + Gcor_COg
+    # G_H2g = E_H2g + 0.274	+ 0.091 - 0.403 + 0.1 + 0
+    # G_CO2g = E_CO2g + 0.306 + 0.099 - 0.664 + 0.3 + 0
+    # G_H2Og = E_H2Og + 0.572 + 0.104 - 0.670
+    # G_COg = E_COg + 0.132 + 0.091 - 0.669
+    
+    # G_gas = E_ZPE + C_harm - TS + Ecorr_overbing + E_solvent
+    Gcor_H = 0.190 + 0.003 - 0.004 + 0 + 0
+    Gcor_HOCO = 0.657 + 0.091 - 0.162 + 0.15 - 0.25
+    Gcor_CO = 0.186 + 0.080 - 0.156 + 0 - 0.10
+    Gcor_OH = 0.355 + 0.056 - 0.103
+    
+    import pdb; pdb.set_trace()
+    ids = []
+    formulas = []
+    sites = []
+    adsors = []
+    energies = []
+    ori_ids = []
+    for row in db.select():
+        uniqueid = row.uniqueid
+        items = uniqueid.split('_')
+        id = items[0]
+        formula = items[1]
+        i_X = formula.find('X')
+        formula = formula[:i_X] + formula[i_X+4:] # remove Xxxx
+        site = items[2]
+        adsor = items[3]
+        ori_id = items[4]
+        
+        ids.append(id)
+        formulas.append(formula)
+        sites.append(site)
+        adsors.append(adsor)
+        energies.append(row.energy)
+        ori_ids.append(ori_id)
+    
+    tuples = {'Id': ids,
+              'Origin_id': ori_ids,
+              'Surface': formulas,
+              'Site': sites,
+              'Adsorbate': adsors,
+              'Energy': energies,
+             }
+    df = pd.DataFrame(tuples)
+    
+    """
+    Save the original data and sorted by adsorbate
+    """
+    uniqueids = df['Origin_id'].astype(int).unique()
+    df_sort  = pd.DataFrame()
+    custom_dict = {'surface':0, 'HOCO': 1, 'CO': 2, 'H': 3, 'OH':4} 
+    for id in uniqueids:
+        df_sub = df.loc[df['Origin_id'].astype(int) == id]
+        df_sub = df_sub.sort_values(by=['Adsorbate'], key=lambda x: x.map(custom_dict))
+        
+        Surface = df_sub.loc[df_sub['Adsorbate'] == 'surface']
+        E_Surface = Surface.Energy.values[0]
+        
+        Binding_energy = []
+        for i,row in df_sub.iterrows():
+            ads = row['Adsorbate']
+            if ads == 'surface':
+                Binding_energy.append(0)
+            elif ads == 'HOCO':
+                E_HOCO = row.Energy
+                Eb_HOCO = E_HOCO - E_Surface - E_CO2g - 0.5 * E_H2g
+                Binding_energy.append(Eb_HOCO)
+            elif ads == 'CO':
+                E_CO = row.Energy
+                Eb_CO = E_CO - E_Surface - E_COg
+                Binding_energy.append(Eb_CO)
+            elif ads == 'H':
+                E_H = row.Energy
+                Eb_H = E_H - E_Surface - 0.5 * E_H2g
+                Binding_energy.append(Eb_H)
+            elif ads == 'OH':
+                E_OH = row.Energy
+                Eb_OH = E_OH - E_Surface - E_H2Og + 0.5 * E_H2g
+                Binding_energy.append(Eb_OH)
+                
+        df_sub['BE'] = Binding_energy
+        df_sort = df_sort.append(df_sub, ignore_index=True)
+    df_sort.to_excel(xls_name, sheet_name='Origin', float_format='%.3f')
+    
+    surfaces = []
+    step_ini = []
+    step_HOCO = []
+    step_CO = []
+    step_final = []
+    Eb_HOCOs = []
+    Eb_COs = []
+    Eb_Hs = []
+    Eb_OHs = []
+    selectivities = []
+    G_HOCOs = []
+    G_COs = []
+    G_Hs = []
+    G_OHs = []
+    FE_final = G_COg + G_H2Og - G_CO2g - G_H2g
+    df_new  = pd.DataFrame()
+    for id in uniqueids:
+        # print(id)
+        df_sub = df.loc[df['Origin_id'].astype(int) == id]
+        Surface = df_sub.loc[df_sub['Adsorbate'] == 'surface']
+        E_Surface = Surface.Energy.values[0]
+        # print(df_sub)
+        HOCOs = df_sub.loc[df_sub['Adsorbate'] == 'HOCO']
+        HOCO = HOCOs[HOCOs.Energy == HOCOs.Energy.min()]
+        E_HOCO = HOCO.Energy.values[0]
+        Eb_HOCO = E_HOCO - E_Surface - E_CO2g - 0.5 * E_H2g
+        G_HOCO = E_HOCO + Gcor_HOCO - E_Surface - G_CO2g - 0.5 * G_H2g
+        
+        COs = df_sub.loc[df_sub['Adsorbate'] == 'CO']
+        CO = COs[COs.Energy == COs.Energy.min()]
+        E_CO = CO.Energy.values[0]
+        Eb_CO = E_CO - E_Surface - E_COg
+        G_CO = E_CO + Gcor_CO + G_H2Og - E_Surface - G_H2g - G_CO2g
+        
+        # print(G_CO-Eb_CO)
+        # >> 0.579
+        # import pdb; pdb.set_trace()
+        
+        Hs = df_sub.loc[df_sub['Adsorbate'] == 'H']
+        H = Hs[Hs.Energy == Hs.Energy.min()]
+        E_H = H.Energy.values[0]
+        Eb_H = E_H - E_Surface - 0.5 * E_H2g
+        G_H = E_H + Gcor_H - E_Surface - 0.5 * G_H2g
+        
+        OHs = df_sub.loc[df_sub['Adsorbate'] == 'OH']
+        OH = OHs[OHs.Energy == OHs.Energy.min()]
+        E_OH = OH.Energy.values[0]
+        Eb_OH = E_OH - E_Surface - E_H2Og + 0.5 * E_H2g
+        G_OH = E_OH + Gcor_OH - E_Surface - G_H2Og + 0.5 * G_H2g
+        
+        Binding_energy = [Eb_HOCO, Eb_CO, Eb_H, Eb_OH]
+        Free_energy = [G_HOCO, G_CO, G_H, G_OH] # free energy according to reaction equations
+        df_stack = pd.concat([HOCO, CO, H, OH], axis=0)
+        df_stack['BE'] = Binding_energy
+        df_stack['FE'] = Free_energy
+        df_new = df_new.append(df_stack, ignore_index=True)
+        
+        surfaces.append(Surface.Surface.values[0])
+        step_ini.append(0)
+        step_HOCO.append(G_HOCO)
+        step_CO.append(G_CO)
+        step_final.append(FE_final)
+        
+        Eb_HOCOs.append(Eb_HOCO)
+        Eb_COs.append(Eb_CO)
+        Eb_Hs.append(Eb_H)
+        Eb_OHs.append(Eb_OH)
+        
+        G_HOCOs.append(G_HOCO)
+        G_COs.append(G_CO)
+        G_Hs.append(G_H)
+        G_OHs.append(G_OH)
+        
+        selectivities.append(G_HOCO - G_H)
+        
+    """
+    Save the most stable site into excel
+    """
+    with pd.ExcelWriter(xls_name, engine='openpyxl', mode='a') as writer:
+        df_new.to_excel(writer, sheet_name='Ori_Stable', float_format='%.3f')
+    
+    """
+    Save free energy sheet to excel
+    """
+    tuples = {'Surface': surfaces,
+              '* + CO2': step_ini,
+              '*HOCO': step_HOCO,
+              '*CO': step_CO,
+              '* + CO': step_final,
+              }
+    df_FE = pd.DataFrame(tuples)
+    with pd.ExcelWriter(xls_name, engine='openpyxl', mode='a') as writer:
+        df_FE.to_excel(writer, sheet_name=sheet_free_energy, index=False, float_format='%.3f')
+        
+    
+    """
+    Save binding energy sheet to excel
+    """
+    tuples = {'Surface': surfaces,
+              'E(*HOCO)': Eb_HOCOs,
+              'E(*CO)': Eb_COs,
+              'E(*H)': Eb_Hs,
+              'E(*OH)': Eb_OHs,
+              }
+    df_BE = pd.DataFrame(tuples)
+    with pd.ExcelWriter(xls_name, engine='openpyxl', mode='a') as writer:
+        df_BE.to_excel(writer, sheet_name=sheet_binding_energy, index=False, float_format='%.3f')
+        
+    """
+    Save all intermediate`s free energy sheet to excel
+    """
+    tuples = {'Surface': surfaces,
+              'G(*HOCO)': G_HOCOs,
+              'G(*CO)': G_COs,
+              'G(*H)': G_Hs,
+              'G(*OH)': G_OHs,
+              }
+    df_all_FE = pd.DataFrame(tuples)
+    with pd.ExcelWriter(xls_name, engine='openpyxl', mode='a') as writer:
+        df_all_FE.to_excel(writer, sheet_name='All_FE', index=False, float_format='%.3f')
+    
+    
+    """
+    Save selectivity sheet to excel
+    """
+    tuples = {'Surface': surfaces,
+              'G_HOCO-G_H': selectivities,
+              }
+    df_select = pd.DataFrame(tuples)
+    with pd.ExcelWriter(xls_name, engine='openpyxl', mode='a') as writer:
+        df_select.to_excel(writer, sheet_name=sheet_selectivity, index=False, float_format='%.3f')
+    
+
+
+def plot_free_enegy(xls_name, sheet_free_energy,):
+    """
+    Plot free energy
+    """
+    df = pd_read_excel(xls_name, sheet_free_energy)
+    step_names = ['* + CO$_{2}$', 'HOCO*', 'CO*', '* + CO']  #reload step name for CO2RR
+    df.set_axis(step_names, axis='columns', inplace=True)
+    name_fig_FE = '../figures/{}_{}.jpg'.format(system_name, sheet_free_energy)
+    fig = plt.figure(figsize=(8, 6), dpi = 300)
+    ax = fig.add_subplot(111)
+    CO2RR_FED = CO2RRFED(df, fig_name=name_fig_FE)
+    CO2RR_FED.plot(ax=ax, save=False, title='')
+    plt.legend(loc = "lower left", bbox_to_anchor=(0.00, -0.50, 0.8, 1.02), ncol=5, borderaxespad=0)
+    plt.show()
+    fig.savefig(name_fig_FE, dpi=300, bbox_inches='tight')
+
+def plot_scaling_relations(xls_name, sheet_binding_energy):
+    """
+    Plot scaling relation by binding energy
+    """
+
+    df = pd_read_excel(xls_name, sheet_binding_energy)
+    col1 = [2, 2, 2, 3, 3, 5] # column in excel
+    col2 = [3, 5, 4, 5, 4, 4] # column in excel
+    
+    fig = plt.figure(figsize=(18, 16), dpi = 300)
+    name_fig_BE = '../figures/{}_{}.jpg'.format(system_name, sheet_binding_energy)
+    M  = 3
+    i = 0
+    for m1 in range(M-1):
+        for m2 in range(M):
+            ax = plt.subplot(M, M, m1*M + m2 + 1)
+            descriper1 = df.columns[col1[i]-2]
+            descriper2 = df.columns[col2[i]-2]
+            sr = ScalingRelation(df, descriper1, descriper2, fig_name=name_fig_BE)
+            sr.plot(ax = ax, save=False,color_ditc=True, title='', xlabel=descriper1, ylabel=descriper2, dot_color='red', line_color='red')
+            i+=1
+    plt.show()
+    fig.savefig(name_fig_BE, dpi=300, bbox_inches='tight')
+
+def concatenate_db(db_name1, db_name2, db_tot):
+    """Contatenate two database into the total one"""
+    
+    if os.path.exists(db_tot):
+        assert False
+    db1 = connect(db_name1)
+    db2 = connect(db_name2)
+    db_tot = connect(db_tot)
+    for row in db1.select():
+        db_tot.write(row)
+    for row in db2.select():
+        db_tot.write(row) 
+
+def plot_stability():
+    """Plot convex hull by formation energy"""
+    ''
+    
+def plot_selectivity(xls_name, sheet_selectivity):
+    """Plot selectivity of CO2RR and HER"""
+    
+    df = pd_read_excel(filename=xls_name, sheet=sheet_selectivity)
+    # df.set_axis(['Single'], axis='columns', inplace=True)
+    name_fig_select = '../figures/{}_{}.jpg'.format(system_name, sheet_selectivity)
+    selectivity = Selectivity(df, fig_name=name_fig_select)
+    selectivity.plot(save=True, title='',xlabel='Different surfaces', tune_tex_pos=1.5, legend=False)
+    
+def plot_activity():
+    """Plot activity of CO2RR"""
+    ''
+
+if __name__ == '__main__':
+    if False:
+        db_tot = '../data/collect_vasp_PdHy_and_Pd32Hy.db'
+        concatenate_db('../data/collect_vasp_PdHy_v3.db', '../data/collect_vasp_Pd32Hy.db', db_tot)
+    
+    system_name = 'collect_vasp_PdHy_v3'
+    # system_name = 'collect_vasp_Pd32Hy'
+    # system_name = 'collect_vasp_PdHy_and_Pd32Hy'
+    db_name = f'../data/{system_name}.db' # the only one needed
+    xls_name = f'../data/{system_name}.xlsx'
+    sheet_free_energy = 'CO2RR_FE'
+    sheet_binding_energy = 'CO2RR_BE'
+    sheet_selectivity = 'Selectivity'
+    db = connect(db_name)
+    if True:
+        db2xls(system_name, xls_name, db, sheet_free_energy, sheet_binding_energy, sheet_selectivity)
+    plot_free_enegy(xls_name, sheet_free_energy,)
+    plot_scaling_relations(xls_name, sheet_binding_energy)
+    plot_selectivity(xls_name, sheet_selectivity)
+    
