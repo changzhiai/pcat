577de989f8230330b549f5afec20c044c7fbae52
Changzhi Ai <changzhiai@outlook.com>
2022-06-27 17:49:27 +0200

    update slurm

--- pcat/utils/slurm.py
+++ pcat/utils/slurm.py
@@ -3,11 +3,26 @@ import os
 
 @contextmanager
 def cd(newdir):
+    """Create and go to the directory"""
     prevdir = os.getcwd()
     try:
         os.makedirs(newdir)
     except OSError:
         pass
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        yield
+    finally:
+        os.chdir(prevdir)
+
+@contextmanager
+def walk(newdir):
+    """Only go to the directory"""
+    prevdir = os.getcwd()
+    # try:
+    #     os.makedirs(newdir)
+    # except OSError:
+    #     pass
     os.chdir(os.path.expanduser(newdir))
     try:
         yield
