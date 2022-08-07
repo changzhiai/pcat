36ee40e678ce4381ed74eefe420fb263ba6bdb1a
Changzhi <changzhiai@outlook.com>
2022-08-07 23:05:05 +0200

    update

--- my_project/proj2/PdTiHy/Convex_hull_dft_PdTiH.py
+++ my_project/proj2/PdTiHy/Convex_hull_dft_PdTiH.py
@@ -659,12 +659,13 @@ if __name__ == '__main__':
     #     db_tot = '../data/collect_vasp_PdHy_and_insert.db'
     #     concatenate_db('../data/collect_vasp_PdHy_v3.db', '../data/collect_vasp_insert_PdHy.db', db_tot)
     
-    for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
-    # for i in [10]:
+    # for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    for i in [9]:
     # for i in [150, 200, 250, 450]:
         # system_name = 'PdTiH_{}'.format(i) # only CE and DFT surface data
         # system_name = 'PdTiH_150' # only CE and DFT surface data
         # system_name = 'PdTiH_surf_r5'
+        # system_name = 'PdTiH_surf_r10plus'
         system_name = 'PdTiH_surf_r{}'.format(i)
         
         metal_obj = 'Ti'
