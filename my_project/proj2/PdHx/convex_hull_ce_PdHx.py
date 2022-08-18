f94f90e785cf3b0f511a75c7ac5cc32855b9576d
Changzhi <changzhiai@outlook.com>
2022-08-18 09:49:17 +0200

    update

--- my_project/proj2/PdHx/convex_hull_ce_PdHx.py
+++ my_project/proj2/PdHx/convex_hull_ce_PdHx.py
@@ -671,8 +671,8 @@ def plot_chem_pot_H_PdHx_discrete():
 
 if __name__ == '__main__':
     
-    # for i in [2, 3, 4, 5, 6, 7, 8]:
-    for i in [8]:
+    for i in [2, 3, 4, 5, 6, 7, 8]:
+    # for i in [8]:
         # system = 'candidates_PdHx'
         # system = 'results_last1'
         # system = 'results_last1_r2'
