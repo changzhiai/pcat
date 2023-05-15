738bed69a5280633efee31a1bb2251e8e4cbc861
Changzhi Ai <changzhiai@outlook.com>
2023-05-15 14:46:39 +0200

    update example

--- examples/plot_free_energy.py
+++ examples/plot_free_energy.py
@@ -10,7 +10,7 @@ Created on Fri Jan  7 15:21:50 2022
 def free_energy_np():
     """Old version to analyze data by numpy"""
     from pcat.lib.io import read_excel
-    from pcat.free_energy import CO2RRFEDplot
+    from pcat.free_energy import CO2RRFEDplot, CO2RRFED_with_TS
     from pcat.utils.styles import StepNamesCO2RR
     
     # step_names, obser_names, X = read_csv(filename, , min_col, max_col) # load csv data
@@ -38,11 +38,20 @@ def free_energy():
     from pcat.lib.io import pd_read_excel
     from pcat.free_energy import CO2RRFED
     
-    df = pd_read_excel(filename='./data/data.xlsx', sheet='free_energy')
+    df = pd_read_excel(filename='./data/data1.xlsx', sheet='free_energy')
     CO2RR_FED = CO2RRFED(df, fig_name='./figures/CO2RR_FED_example.jpg')
     CO2RR_FED.plot(save=True, title='')
 
+def free_energy_TS():
+    """New version to analyze data by pandas"""
+    from pcat.lib.io import pd_read_excel
+    from pcat.free_energy import CO2RRFED, CO2RRFED_TS
+    
+    df = pd_read_excel(filename='./data/data_TS.xlsx', sheet='ex1')
+    CO2RR_FED = CO2RRFED_TS(df, fig_name='./figures/CO2RR_FED_example1.jpg')
+    CO2RR_FED.plot(save=False, title='')
+
 if __name__ == '__main__':
-    free_energy()
+    free_energy_TS()
 
 
